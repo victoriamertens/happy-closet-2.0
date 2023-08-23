891066d627c215c9a16fa4282c2e05b6c3b8b25e
Victoria Mertens <victoria.mertens44@gmail.com>
2023-08-23 14:55:48 -0500

    Script for seeding dev data written to test relational database functioning.

--- prisma/scripts.ts
+++ prisma/scripts.ts
@@ -2,23 +2,46 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const outfits = await prisma.outfits.create({
+  //first Entry of test data
+  const outfit1 = await prisma.outfits.create({
     data: {
       user_id: "abcd1234",
       outfit_image_url: "http://www.outfitURL.com",
       items: {
         create: [
-          {
-            user_id: "abcd1234",
-            image_url: "http://www.itemURL1.com",
-          },
+          { user_id: "abcd1234", image_url: "http://www.itemURL1.com" },
           { user_id: "abcd1234", image_url: "http://www.itemURL2.com" },
+          { user_id: "abcd1234", image_url: "http://www.itemURL3.com" },
+        ],
+      },
+      wearlog: {
+        create: [{ user_id: "abcd1234", reaction: "Dislike" }],
+      },
+    },
+  });
+
+  //Second Entry of test data
+  const outfit2 = await prisma.outfits.create({
+    data: {
+      user_id: "xyz999",
+      outfit_image_url: "http://www.outfit2URL.com",
+      items: {
+        create: [
+          { user_id: "xyz999", image_url: "http://www.2itemURL1.com" },
+          { user_id: "xyz999", image_url: "http://www.2itemURL2.com" },
+        ],
+      },
+      wearlog: {
+        create: [
+          { user_id: "xyz999", reaction: "Like" },
+          { user_id: "xyz999", reaction: "Dislike" },
         ],
       },
     },
   });
 
-  console.log(outfits);
+  console.log(outfit1);
+  console.log(outfit2);
 }
 
 main()
