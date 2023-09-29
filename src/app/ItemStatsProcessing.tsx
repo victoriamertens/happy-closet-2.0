7f253b9b05c8b1418ab38887c4bca6a54238b719
Victoria Mertens <victoria.mertens44@gmail.com>
2023-09-29 11:51:22 -0500

    Prisma Client queries for single item bundled in single Transaction

--- src/app/ItemStatsProcessing.tsx
+++ src/app/ItemStatsProcessing.tsx
@@ -6,49 +6,57 @@ import { get } from "http";
 export default async function ItemStatsProcessing() {
   let user = auth();
 
-  const getStats = async (userId: string) => {
-    let single_item = { where: { user_id: userId } };
-
+  const getStatsFromPrisma = async (userId: string) => {
     try {
-      let item = await prisma.items.findFirst(single_item);
-      let processedItem = JSON.stringify(item);
+      let single_item_query = { where: { user_id: userId } };
+      let item = await prisma.items.findFirst(single_item_query);
 
-      const countDifferentOutfit = await prisma.outfits.count({
-        where: { items: { some: { id: item?.id } } },
-      });
-
-      const wears = await prisma.outfit_WearLogs.count({
-        where: { outfit: { items: { some: { id: item?.id } } } },
-      });
+      const [
+        differentOutfitCount,
+        itemWearCount,
+        itemsPairedWith,
+        allItemsRanking,
+      ] = await prisma.$transaction([
+        //differentOutfitCount = number of distinct outfits item is included in
+        prisma.outfits.count({
+          where: { items: { some: { id: item?.id } } },
+        }),
+        //itemWearCount
+        prisma.outfit_WearLogs.count({
+          where: { outfit: { items: { some: { id: item?.id } } } },
+        }),
+        //itemsPairedWith
+        prisma.items.findMany({
+          where: {
+            outfits: {
+              some: { items: { some: { id: { equals: item?.id } } } },
+            },
+          },
+        }),
+        //allItemsRanking
+        prisma.items.findMany({
+          include: {
+            outfits: {
+              include: { _count: { select: { wearlog: true } } },
+            },
+          },
+        }),
+      ]);
 
       let costPerWear;
       {
         item.inital_cost
-          ? (costPerWear = item.initial_cost / wears)
+          ? (costPerWear = item.initial_cost / itemWearCount)
           : (costPerWear = 0);
       }
-
-      const itemsPairedWith = await prisma.items.findMany({
-        where: {
-          outfits: { some: { items: { some: { id: { equals: item?.id } } } } },
-        },
-      });
-
-      let allItemsRanking = await prisma.items.findMany({
-        include: {
-          outfits: {
-            include: { _count: { select: { wearlog: true } } },
-          },
-        },
-      });
 
       let itemColorPairs: any[] = [];
       itemsPairedWith.map(
         (itemPairedWith) => itemColorPairs?.push(itemPairedWith.color),
       );
 
-      console.log("HERE:", JSON.stringify(allItemsRanking));
-      return <p className="userHERE">Data from database: {processedItem}</p>;
+      console.log("HERE:", itemWearCount);
+      return <p className="userHERE">Data from database: {itemWearCount}</p>;
     } catch (err) {
       console.log(err);
       return "Error: No Data in database to fetch";
@@ -56,7 +64,7 @@ export default async function ItemStatsProcessing() {
   };
 
   if (user.sessionId) {
-    const data = await getStats("abcd1234");
+    const data = await getStatsFromPrisma("abcd1234");
     return data;
   } else {
     return <p>Not signed in, cannot access application data.</p>;
