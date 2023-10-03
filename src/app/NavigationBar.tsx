b17fe325c3201ee9e1e31c49ed06a638a3ab1cf6
Victoria Mertens <victoria.mertens44@gmail.com>
2023-10-03 09:14:15 -0500

    All icons for Navigation Bar imported into project

--- src/app/NavigationBar.tsx
+++ src/app/NavigationBar.tsx
@@ -1,4 +1,7 @@
 import HomeButton from "../../public/home-icon-silhouette.svg";
+import AddButton from "../../public/plus-circle-svgrepo-com.svg";
+import ClosetButton from "../../public/closet-svgrepo-com.svg";
+
 import Image from "next/image";
 
 export default function NavigationBar() {
@@ -10,6 +13,20 @@ export default function NavigationBar() {
           className="w-10 h-10"
           height={100}
           width={100}
+          alt="Navigation Button: to Home"
+        />
+        <Image
+          src={AddButton}
+          className="w-10 h-10"
+          height={90}
+          width={90}
+          alt="Navigation Button: to Home"
+        />
+        <Image
+          src={ClosetButton}
+          className="w-10 h-10"
+          height={90}
+          width={90}
           alt="Navigation Button: to Home"
         />
       </div>
