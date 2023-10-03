1f1bd5199f16b1f5aed64a7f14549716b79d0403
Victoria Mertens <victoria.mertens44@gmail.com>
2023-10-03 09:07:39 -0500

    HomeButton with SVG in project/

--- src/app/NavigationBar.tsx
+++ src/app/NavigationBar.tsx
@@ -1,7 +1,18 @@
+import HomeButton from "../../public/home-icon-silhouette.svg";
+import Image from "next/image";
+
 export default function NavigationBar() {
   return (
-    <div>
-      <p>Testing Navigation Bar for Application</p>
-    </div>
+    <section>
+      <div>
+        <Image
+          src={HomeButton}
+          className="w-10 h-10"
+          height={100}
+          width={100}
+          alt="Navigation Button: to Home"
+        />
+      </div>
+    </section>
   );
 }
