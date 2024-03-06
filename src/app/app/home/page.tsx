c67d8e406342d8211e2857e75ec3cbac30942741
Victoria Mertens <victoria.mertens44@gmail.com>
2024-03-06 08:19:19 -0600

    URL redirect for after signout

--- src/app/app/home/page.tsx
+++ src/app/app/home/page.tsx
@@ -1,10 +1,10 @@
-import { SignOutButton } from "@clerk/nextjs";
+import { SignOutButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-4xl font-bold m-5"> Happy Closet Homepage</h1>
-      <SignOutButton />
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
