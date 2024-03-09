eb9633cc411f5b6178c5cd8ef0308b64e7ff734b
Victoria Mertens <victoria.mertens44@gmail.com>
2024-03-09 09:35:16 -0600

    Structure and styling for Clerk SignIn buttons, wrapped with styles

--- src/app/_lib/ClerkAuth/ClerkSignInButtons.tsx
+++ src/app/_lib/ClerkAuth/ClerkSignInButtons.tsx
@@ -1,9 +1,24 @@
 import { SignInButton, auth, SignUpButton } from "@clerk/nextjs";
+import { MainBtnStyling } from "../TailwindWrappers/MainBtnStyling";
 
 export default function ClerkSignInButtons() {
   return (
-    <div>
-      <SignInButton mode="redirect" /> <SignUpButton mode="redirect" />
+    <div id="signInPageBtns" className="flex flex-col items-center">
+      <div id="SignInBtn">
+        <MainBtnStyling>
+          <SignInButton mode="redirect">
+            <button className="font-bold">Sign in</button>
+          </SignInButton>
+        </MainBtnStyling>
+      </div>
+      <div id="signUpBtn">
+        <MainBtnStyling>
+          <SignUpButton mode="redirect">
+            <button className="font-bold">Sign up</button>
+          </SignUpButton>
+        </MainBtnStyling>
+      </div>
+      <p className="m-2 text-xs">Google Authentication</p>
     </div>
   );
 }
