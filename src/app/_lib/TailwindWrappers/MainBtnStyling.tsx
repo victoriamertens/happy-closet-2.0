de1407f21a2538163fb0fbadb0c905046c91ec1e
Victoria Mertens <victoria.mertens44@gmail.com>
2024-03-09 18:41:34 -0600

    Dynamic styling of sign in button for main and secondary styling

--- src/app/_lib/TailwindWrappers/MainBtnStyling.tsx
+++ src/app/_lib/TailwindWrappers/MainBtnStyling.tsx
@@ -1,10 +1,27 @@
 import React, { FC, PropsWithChildren } from "react";
 
-interface MainBtnStylingProps extends PropsWithChildren {}
+interface MainBtnStylingProps extends PropsWithChildren {
+  btnType: string;
+}
 
-export const MainBtnStyling: FC<MainBtnStylingProps> = ({ children }) => {
+export const MainBtnStyling: FC<MainBtnStylingProps> = ({
+  children,
+  btnType,
+}) => {
+  let dynamicStyling;
+  if (btnType === "main") {
+    dynamicStyling = "bg-teal-400 text-teal-900 hover:bg-teal-500";
+  } else if (btnType === "secondary") {
+    dynamicStyling = "border-black hover:bg-teal-100";
+  }
+
   return (
-    <div className=" m-2 flex w-72 flex-row justify-center rounded-3xl border-2 border-black p-2">
+    <div
+      className={
+        " m-2 flex w-72 flex-row justify-center rounded-3xl border-2 shadow-2xl " +
+        dynamicStyling
+      }
+    >
       {children}
     </div>
   );
