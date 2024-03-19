5f0d6bdc0da59f10df88561a3cc327afc3573712
Victoria Mertens <victoria.mertens44@gmail.com>
2024-03-19 17:16:47 -0500

    Updated scripts to test functionality of new query

--- prisma/scripts.ts
+++ prisma/scripts.ts
@@ -1,47 +1,67 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+import { createItem } from "./queries";
+import { itemType } from "./queries";
 
 async function main() {
   //first Entry of test data
-  const outfit1 = await prisma.outfits.create({
-    data: {
-      user_id: "abcd1234",
-      outfit_image_url: "http://www.outfitURL.com",
-      items: {
-        create: [
-          { user_id: "abcd1234", image_url: "http://www.itemURL1.com" },
-          { user_id: "abcd1234", image_url: "http://www.itemURL2.com" },
-          { user_id: "abcd1234", image_url: "http://www.itemURL3.com" },
-        ],
-      },
-      wearlog: {
-        create: [{ user_id: "abcd1234", reaction: "Dislike" }],
-      },
-    },
-  });
+
+  // const outfit1 = await prisma.outfits.create({
+  //   data: {
+  //     user_id: "abcd1234",
+  //     outfit_image_url: "http://www.outfitURL.com",
+  //     items: {
+  //       create: [
+  //         { user_id: "abcd1234", image_url: "http://www.itemURL1.com" },
+  //         { user_id: "abcd1234", image_url: "http://www.itemURL2.com" },
+  //         { user_id: "abcd1234", image_url: "http://www.itemURL3.com" },
+  //       ],
+  //     },
+  //     wearlog: {
+  //       create: [{ user_id: "abcd1234", reaction: "Dislike" }],
+  //     },
+  //   },
+  // });
 
   //Second Entry of test data
-  const outfit2 = await prisma.outfits.create({
-    data: {
-      user_id: "xyz999",
-      outfit_image_url: "http://www.outfit2URL.com",
-      items: {
-        create: [
-          { user_id: "xyz999", image_url: "http://www.2itemURL1.com" },
-          { user_id: "xyz999", image_url: "http://www.2itemURL2.com" },
-        ],
-      },
-      wearlog: {
-        create: [
-          { user_id: "xyz999", reaction: "Like" },
-          { user_id: "xyz999", reaction: "Dislike" },
-        ],
-      },
-    },
-  });
+  // const outfit2 = await prisma.outfits.create({
+  //   data: {
+  //     user_id: "xyz999",
+  //     outfit_image_url: "http://www.outfit2URL.com",
+  //     items: {
+  //       create: [
+  //         { user_id: "xyz999", image_url: "http://www.2itemURL1.com" },
+  //         { user_id: "xyz999", image_url: "http://www.2itemURL2.com" },
+  //       ],
+  //     },
+  //     wearlog: {
+  //       create: [
+  //         { user_id: "xyz999", reaction: "Like" },
+  //         { user_id: "xyz999", reaction: "Dislike" },
+  //       ],
+  //     },
+  //   },
+  // });
 
-  console.log(outfit1);
-  console.log(outfit2);
+  //Run the Functions for the scripts
+  const testItem: itemType = {
+    user_id: "1",
+    name: "testName",
+    date_added: new Date(),
+    color: "blue",
+    image_url: "test.com",
+    initial_cost: 60,
+    is_secondhand: true,
+    is_instorepurchase: false,
+    is_airdry: false,
+    is_drycleanonly: false,
+    is_coldwash: false,
+    is_justpurchased: false,
+  };
+
+  // console.log(outfit1);
+  // console.log(outfit2);
+  createItem(testItem);
 }
 
 main()
