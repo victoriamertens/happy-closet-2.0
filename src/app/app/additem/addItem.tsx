0b58d066a561fee6bdd0bc296696ee43809ec39c
Victoria Mertens <victoria.mertens44@gmail.com>
2024-03-25 18:33:36 -0500

    Pulled info for prisma query from body of request

--- src/app/app/additem/addItem.tsx
+++ src/app/app/additem/addItem.tsx
@@ -1,22 +1,12 @@
 "use server";
 import { createItem, ItemType } from "../../../../prisma/queries";
 
-export const addItem = () => {
-  console.log("In addItem Function");
-  let body: ItemType = {
-    user_id: "2",
-    name: "testCreateItemName",
-    date_added: new Date(),
-    color: "white",
-    image_url: "testCreate.com",
-    initial_cost: 60,
-    is_secondhand: true,
-    is_instorepurchase: false,
-    is_airdry: false,
-    is_drycleanonly: false,
-    is_coldwash: false,
-    is_justpurchased: false,
-  };
-
-  createItem(body);
+export const addItem = (uploadItemBody: ItemType) => {
+  try {
+    console.log("In addItem Function");
+    //Server Logic goes here for addItem
+    createItem(uploadItemBody);
+  } catch (err) {
+    console.log("Error on addItem in server:", err);
+  }
 };
