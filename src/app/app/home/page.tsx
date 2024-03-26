65149594dc49e2ec44f506b17ecba04a934aee67
Victoria Mertens <victoria.mertens44@gmail.com>
2024-03-26 08:09:23 -0500

    Deleted UserButton bc moved to layout level of app:

--- src/app/app/home/page.tsx
+++ src/app/app/home/page.tsx
@@ -3,8 +3,7 @@ import { SignOutButton, UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-4xl font-bold m-5"> Happy Closet Homepage</h1>
-      <UserButton afterSignOutUrl="/" />
+      <h1 className="m-5 text-4xl font-bold"> Happy Closet Homepage</h1>
     </div>
   );
 }
