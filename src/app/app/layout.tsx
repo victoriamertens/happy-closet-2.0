96d41db207a5554a655e8b86069ce8ee52433889
Victoria Mertens <victoria.mertens44@gmail.com>
2024-03-26 07:36:49 -0500

    User Component from Clerk at layout level of App

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,8 +1,10 @@
 import NavigationBar from "../_lib/NavigationBar";
+import { SignOutButton, UserButton } from "@clerk/nextjs";
 
 export default function App({ children }: { children: React.ReactNode }) {
   return (
     <section>
+      <UserButton afterSignOutUrl="/" />
       {children}
       <NavigationBar />
     </section>
