70499e1d2fdc22ae9c6d1f5c1be8d11909d13b9e
Victoria Mertens <victoria.mertens44@gmail.com>
2024-03-28 08:39:39 -0500

    MainButtonStyling more dynamic

--- src/app/_lib/ClerkAuth/ClerkSignInButtons.tsx
+++ src/app/_lib/ClerkAuth/ClerkSignInButtons.tsx
@@ -6,12 +6,12 @@ export default function ClerkSignInButtons() {
     <div id="signInPageBtns" className="flex flex-col items-center">
       <div id="SignInBtn">
         <SignInButton mode="redirect">
-          <MainBtnStyling btnType="main" />
+          <MainBtnStyling btnType="main" buttonText="Sign In" />
         </SignInButton>
       </div>
       <div id="signUpBtn">
         <SignUpButton mode="redirect">
-          <MainBtnStyling btnType="secondary" />
+          <MainBtnStyling btnType="secondary" buttonText="Sign Up" />
         </SignUpButton>
       </div>
       <p className="m-2 text-xs">Google Authentication</p>
