fa87221e9abedac1b694c25978dd91091526ace8
Victoria Mertens <victoria.mertens44@gmail.com>
2024-03-28 08:42:04 -0500

    Componetized Input Styling for text/number input types

--- src/app/_lib/TailwindWrappers/InputStyling.tsx
+++ src/app/_lib/TailwindWrappers/InputStyling.tsx
@@ -1,7 +1,27 @@
-import React, { ChangeEventHandler, FC, PropsWithChildren } from "react";
+import React, {
+  ChangeEventHandler,
+  FC,
+  InputHTMLAttributes,
+  PropsWithChildren,
+} from "react";
 
-interface InputStylingProps extends PropsWithChildren {}
+interface InputStylingProps extends InputHTMLAttributes<HTMLInputElement> {
+  placeholder: string;
+  type: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+}
 
-export const InputStyling: FC<InputStylingProps> = ({ children }) => {
-  return <div className="bg-slate-500 p-10">{children}</div>;
+export const InputStyling: FC<InputStylingProps> = ({
+  children,
+  placeholder,
+  ...rest
+}) => {
+  return (
+    <div className="relative my-5 w-full border border-black ">
+      <span className="absolute left-3 top-[-20px] m-0 bg-white px-2  text-gray-400">
+        {placeholder}
+      </span>
+      <input {...rest} className="h-10 w-full px-4"></input>
+    </div>
+  );
 };
