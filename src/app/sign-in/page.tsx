f87650379d40658db1f40acb86d8943abcfa9125
Victoria Mertens <victoria.mertens44@gmail.com>
2024-04-05 10:16:17 -0500

    fix auth

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 export default function SignIn() {
   let { sessionId } = auth();
 
-  let SignIn = Clerk.SignIn;
+  let ClerkSignIn = Clerk.SignIn;
 
   if (sessionId) {
     redirect("/app/home");
@@ -13,7 +13,7 @@ export default function SignIn() {
 
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <SignIn />
+      <ClerkSignIn />
     </div>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,7 @@
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "sign-in", "/about"],
-  afterAuth(auth, req, evt) {
-    if (auth.sessionId && req.nextUrl.pathname) {
-    }
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({
-        returnBackUrl: req.experimental_clerkUrl.origin,
-      });
-    }
-  },
+  publicRoutes: ["/", "/about"],
 });
 
 export const config = {
