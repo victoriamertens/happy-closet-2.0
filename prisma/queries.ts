dc4f2bca5a0c1f8690c2262e10030cb4553984f0
Victoria Mertens <victoria.mertens44@gmail.com>
2024-04-14 14:32:54 -0500

    Updated query and item type to match database refactor for Dev MVP

--- prisma/queries.ts
+++ prisma/queries.ts
@@ -7,11 +7,11 @@ export type ItemType = {
   item_name: string;
   date_added: Date;
   primary_color: string;
-  secondary_color: String;
-  tertiary_color: String;
-  patterned_item: Boolean;
-  pattern_type: String;
-  description_word: String;
+  secondary_color: string;
+  tertiary_color: string;
+  patterned_item: boolean;
+  pattern_type: string;
+  description_word: string;
   image_url: string;
   initial_cost: number;
   is_secondhand: boolean;
@@ -25,17 +25,19 @@ const createItem = async (item: ItemType) => {
     const newItem = await prisma.items.create({
       data: {
         user_id: item.user_id,
-        name: item.name,
+        item_name: item.item_name,
         date_added: new Date(),
-        color: item.color,
+        primary_color: item.primary_color,
         image_url: item.image_url,
         initial_cost: item.initial_cost,
         is_secondhand: item.is_secondhand,
-        is_instorepurchase: item.is_instorepurchase,
-        is_airdry: item.is_airdry,
-        is_drycleanonly: item.is_drycleanonly,
-        is_coldwash: item.is_coldwash,
         is_justpurchased: item.is_justpurchased,
+        secondary_color: item.secondary_color,
+        tertiary_color: item.tertiary_color,
+        purchased_date: new Date(),
+        patterned_item: item.patterned_item,
+        pattern_type: item.pattern_type,
+        description_word: item.description_word,
       },
     });
     console.log("New item created:", newItem);
