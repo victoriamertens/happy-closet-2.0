53c7f63a1098e931ba973222df736d2fb3001dfc
Victoria Mertens <victoria.mertens44@gmail.com>
2024-04-17 08:42:13 -0500

    Updated middleware to function properly by elkiminating postman accessibility hack

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/about", "/api/items"],
-  ignoredRoutes: ["/((?!api|trpc))(_next|.+..+)(.*)", "/api/item"],
 });
 
 export const config = {
