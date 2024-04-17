d59ebbb638ce93119fa07e224682a99e073168eb
Victoria Mertens <victoria.mertens44@gmail.com>
2024-04-17 08:43:16 -0500

    Added item read all to project in server component, rendering DB info as expected

--- src/app/app/closet/page.tsx
+++ src/app/app/closet/page.tsx
@@ -1,3 +1,22 @@
-export default function Closet() {
-  return <p> Closet Page</p>;
+import { useUser, currentUser } from "@clerk/nextjs";
+import { PrismaClient } from "@prisma/client";
+
+export default async function Closet() {
+  const user = await currentUser();
+  const prisma = new PrismaClient();
+
+  if (user) {
+    try {
+      const allItems = await prisma.items.findMany({
+        where: { user_id: user.id },
+      });
+      return JSON.stringify(allItems);
+    } catch (error) {
+      console.error("Error fetching allitems:", error);
+      return <p>No Items in Database</p>;
+    }
+  } else {
+    //Design decision, redirect or have this catch? With Middleware, this should never be hit anyway
+    return <p>Not Signed In</p>;
+  }
 }
