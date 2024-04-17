11ad7494c5c0b34d019401c9c0517a172efd744e
Victoria Mertens <victoria.mertens44@gmail.com>
2024-04-17 08:43:51 -0500

    Updated Navigation Bar to allow navigation to closet page, will update styling in future

--- src/app/_lib/NavigationBar.tsx
+++ src/app/_lib/NavigationBar.tsx
@@ -27,25 +27,24 @@ export default function NavigationBar() {
             alt="Add action"
           />
         </RouteToButton>
-        {/* <RouteToButton newUrl="app/closet"> */}
-
-        <div className="group relative flex">
-          <Image
-            src={ClosetButton}
-            className="h-10 w-10"
-            height={90}
-            width={90}
-            alt="View action"
-            data-tooltip-target="closetbtn"
-          />
-          <span
-            className="absolute left-1/2 m-4 mx-auto -translate-y-5 translate-x-1/2 rounded-md bg-gray-800 px-1 
+        <RouteToButton newUrl="app/closet">
+          <div className="group relative flex">
+            <Image
+              src={ClosetButton}
+              className="h-10 w-10"
+              height={90}
+              width={90}
+              alt="View action"
+              data-tooltip-target="closetbtn"
+            />
+            <span
+              className="absolute left-1/2 m-4 mx-auto -translate-y-5 translate-x-1/2 rounded-md bg-gray-800 px-1 
     text-sm text-gray-100 opacity-0 transition-opacity group-hover:opacity-100"
-          >
-            Coming Soon
-          </span>
-        </div>
-        {/* </RouteToButton> */}
+            >
+              Coming Soon
+            </span>
+          </div>
+        </RouteToButton>
       </div>
     </section>
   );
