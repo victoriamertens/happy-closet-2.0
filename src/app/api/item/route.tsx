7bdba408ac6aced208bab60235e7b0f739206f47
Victoria Mertens <victoria.mertens44@gmail.com>
2024-05-04 10:11:17 -0500

    Added PUT method for archive of item

--- src/app/api/item/route.tsx
+++ src/app/api/item/route.tsx
@@ -3,6 +3,9 @@ import { createItem, fetchAllItems } from "../../../../prisma/queries";
 import { NextResponse, NextRequest } from "next/server";
 // import CurrentUser from "@/app/_lib/ClerkAuth/CurrentUser";
 import { currentUser, auth } from "@clerk/nextjs";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function POST(request: NextRequest) {
   console.log("In post for addItem");
@@ -15,6 +18,33 @@ export async function POST(request: NextRequest) {
     console.log("Error in POST addItem:", err);
     return NextResponse.json(
       { message: "Error adding item to Database" },
+      { status: 500 },
+    );
+  }
+}
+export async function PUT(request: NextRequest) {
+  const requestId: undefined | number = await request.json();
+  console.log("In delete for Item", requestId);
+
+  try {
+    const updatedRecord = await prisma.items.update({
+      where: {
+        id: Number(requestId),
+      },
+      data: {
+        in_closet: false,
+      },
+    });
+
+    console.log("Updated record:", updatedRecord);
+    return NextResponse.json(
+      { message: "Updated record in DB:" + updatedRecord },
+      { status: 200 },
+    );
+  } catch (err) {
+    console.log("Error in POST addItem:", err);
+    return NextResponse.json(
+      { message: "Error updating item to Database" },
       { status: 500 },
     );
   }
